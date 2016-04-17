d6e57b484797097aeeffd65d48e436a493256bec
PedroSales <pedrosal3s@gmail.com>
2016-04-17 15:07:05 -0300

    Implementando funcao de tratamento de entrada e numero maximo de erros

--- forca.hs
+++ forca.hs
@@ -1,5 +1,7 @@
 import Data.List (transpose)
 
+
+--Matriz com imagens dos bonecos da forca em uma matriz transposta
 imagensBonecoForca :: [[String]]
 imagensBonecoForca =
 	transpose
@@ -7,6 +9,7 @@ imagensBonecoForca =
 	,["   ", "   ", " | ", " | ", " | " , "/| " , " |\\" ]
 	,["   ", "   ", "   ", "/  ", "/ \\", "/ \\", "/ \\"]]
 
+--Função para renderizar boneco na forca, de acordo com indice, que receberá numero de erros
 imagemForca :: Int -> [String]
 imagemForca indice = 
 	"-----------" :
@@ -14,3 +17,14 @@ imagemForca indice =
 	map ("|   " ++) imagem
 	where imagem = imagensBonecoForca !! indice
 
+--Declaração do número máximo de erros
+numeroMaxErros :: Int
+numeroMaxErros = length imagensBonecoForca - 1
+
+--função para pegar caracter digitado, observando e tratando a entrada do usuário
+getLetra :: IO Char
+getLetra = do
+	linha <- getLine
+	case linha of 
+		[] -> getLetra
+		(c:_) -> return c
